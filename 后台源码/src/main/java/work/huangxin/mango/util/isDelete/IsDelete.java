d8cb75c98140a8d5523f0dae938a39847b86a903
黄鑫 <40482811+Xin-Felix@users.noreply.github.com>
2020-03-31 17:02:12 +0800

    Update IsDelete.java

--- 后台源码/src/main/java/work/huangxin/mango/util/isDelete/IsDelete.java
+++ 后台源码/src/main/java/work/huangxin/mango/util/isDelete/IsDelete.java
@@ -76,9 +76,9 @@ public class IsDelete {
             // Endpoint以杭州为例，其它Region请按实际情况填写。
             String endpoint = "oss-cn-huhehaote.aliyuncs.com";
             // 阿里云主账号AccessKey拥有所有API的访问权限，风险很高。强烈建议您创建并使用RAM账号进行API访问或日常运维，请登录 https://ram.console.aliyun.com 创建RAM账号。
-            String accessKeyId = "LTAIekEJFiF5CpnT";
-            String accessKeySecret = "2bi4xbkuMPR8n2cfEEqruyTIrPit9c";
-            String bucketName = "1538933906";
+            String accessKeyId = "你的阿里云id";
+            String accessKeySecret = "你的阿里云密钥";
+            String bucketName = "oss名称";
 
 
             // 创建OSSClient实例。
